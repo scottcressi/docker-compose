173fd85322b147b307b364db31c693720b4dcf7d
Scott Cressi <scottcressi@gmail.com>
2021-03-11 21:29:35 -0500

    no ethhtool, no vol, telegraf binary

---
+++ monitoring/client.sh
@@ -0,0 +1,3 @@
+wget https://dl.influxdata.com/telegraf/releases/telegraf-1.17.3_linux_amd64.tar.gz
+tar xf telegraf-1.17.3_linux_amd64.tar.gz
+./telegraf-1.17.3/usr/bin/telegraf --config telegraf.conf
