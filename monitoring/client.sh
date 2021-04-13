becb729dd912dc31ead441b1eb33f82021280f39
Scott Cressi <scottcressi@gmail.com>
2021-04-12 22:12:15 -0400

    update

--- monitoring/client.sh
+++ monitoring/client.sh
@@ -1,3 +1,3 @@
-wget -nc https://dl.influxdata.com/telegraf/releases/telegraf-1.17.3_linux_amd64.tar.gz
-tar xf telegraf-1.17.3_linux_amd64.tar.gz
-./telegraf-1.17.3/usr/bin/telegraf --config telegraf.conf
+wget -nc https://dl.influxdata.com/telegraf/releases/telegraf-1.18.1_linux_amd64.tar.gz
+tar xf telegraf-1.18.1_linux_amd64.tar.gz
+./telegraf-1.18.1/usr/bin/telegraf --config telegraf.conf
