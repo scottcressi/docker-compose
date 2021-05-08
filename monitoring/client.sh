2386816449de239f41350a780e5837f13b74b1a2
Scott Cressi <scottcressi@gmail.com>
2021-05-07 21:26:00 -0400

    single conf, version

--- monitoring/client.sh
+++ monitoring/client.sh
@@ -1,13 +1,13 @@
 #!/usr/local/env sh
 
 url=https://dl.influxdata.com/telegraf/releases
-binary=telegraf-1.18.1_linux_amd64.tar.gz
-binaryhash=9983cbb853514f1940d237bb8ebd10d68ee6be051e560af04f19473d1e058fdc
+binary=telegraf-1.18.2_linux_amd64.tar.gz
+binaryhash=9a01d0351b3bafd94ada8278f191271242cd12122c22b4ec44fdc55ae062fc4a
 wget -nc $url/$binary
 if [ "$binaryhash" = "$(sha256sum $binary | awk '{print $1}')" ] ; then
     echo hash match ; echo
-    tar xf telegraf-1.18.1_linux_amd64.tar.gz
-    ./telegraf-1.18.1/usr/bin/telegraf --config telegraf.conf
+    tar xf telegraf-1.18.2_linux_amd64.tar.gz
+    ./telegraf-1.18.2/usr/bin/telegraf --config telegraf.conf
 else
     echo hash not match ; echo
     rm -f $binary
